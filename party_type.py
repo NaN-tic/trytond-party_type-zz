e12504a6ee33108d936616584f1ebdc6a9e3e7ac
Udo Spallek <udono@virtual-things.biz>
2008-11-25 14:35:10 +0100

    Cleanup: remove complete_person_name, removing 'person_' suffix from variable names.

--- party_type.py
+++ party_type.py
@@ -7,14 +7,12 @@ _STATES_PERSON = {
     "readonly": "active == False",
     "invisible": "type != 'person'",
 }
-_STATES_PARTY = {
-    "readonly": "active == False",
-}
+
 _CHANGE_NAME_FIELDS = [
     "type",
     "name_order",
-    "person_first_name",
-    "person_last_name",
+    "first_name",
+    "last_name",
 ]
 
 
@@ -34,24 +32,21 @@ class PartyType(OSV):
             states={"readonly": "active == False"})
     name = fields.Char("Name", size=None, required=True, select=1,
             states={"readonly": "active == False",
-                    "invisible": "type == 'person'"})
-    complete_person_name = fields.Function("get_complete_person_name",
-            type="char", string="Name",
-            states={"readonly": "active == False",
-                    "invisible": "type != 'person'"})
-    person_gender = fields.Selection(
+                    "readonly": "type == 'person'"})
+    gender = fields.Selection(
             [("male", "Male"),
              ("female", "Female"),
             ], "Gender", select=1, readonly=False, states=_STATES_PERSON)
-    person_last_name = fields.Char("Last Name", size=None,
+    last_name = fields.Char("Last Name", size=None,
             required=False, on_change=_CHANGE_NAME_FIELDS,
             states=_STATES_PERSON)
-    person_first_name = fields.Char("First Name", size=None,
+    first_name = fields.Char("First Name", size=None,
             required=False, on_change=_CHANGE_NAME_FIELDS,
             states=_STATES_PERSON)
     name_order = fields.Property(type="selection", selection=
-            [("first_last", "Firstname Lastname"),
-             ("last_first", "Lastname, Firstname")
+            [("first_last", "<First-Name> <Last-Name>"),
+             ("last_first", "<Last-Name> <First-Name>"),
+             ("last_comma_first", "<Last-Name>, <First-Name>")
             ], string="Order", on_change=_CHANGE_NAME_FIELDS, required=True,
             states=_STATES_PERSON, help="The order of the name parts which " \
                     "build the party name of a person.")
@@ -68,7 +63,7 @@ class PartyType(OSV):
     def default_name_order(self, cursor, user, context=None):
         return "last_first"
 
-    def default_person_gender(self, cursor, user, context=None):
+    def default_gender(self, cursor, user, context=None):
         return "male"
 
     # This method (re-)build the name attribute for the records
@@ -80,18 +75,21 @@ class PartyType(OSV):
         res = {}
 
         if vals["type"] == "person":
-            # TODO: Make an config for the party module where the user can flexible provide
-            # python-code to generate the partyname entry
             print "vals:", vals
-            first_name = vals["person_first_name"] or ''
-            last_name = vals["person_last_name"] or ''
+            first_name = vals["first_name"] or ''
+            last_name = vals["last_name"] or ''
             if vals["name_order"] == "first_last":
-                res["name"] = res["complete_person_name"] = \
+                res["name"] = \
                         first_name \
                         + (last_name and first_name and ' ' or '') \
                         + last_name
             elif vals["name_order"] == "last_first":
-                res["name"] = res["complete_person_name"] = \
+                res["name"] = \
+                        last_name \
+                        + (last_name and first_name and ' ' or '') \
+                        + first_name
+            elif vals["name_order"] == "last_comma_first":
+                res["name"] = \
                         last_name \
                         + (last_name and first_name and ', ' or '') \
                         + first_name
@@ -99,11 +97,9 @@ class PartyType(OSV):
                 pass
         return res
 
-    #### Object Methods
     def on_change_type(self, cursor, user, ids, vals, context=None):
-        # On change of the type from person to organization or reversed,
-        #  we need to delete all the other types data to prevent errors
-        #  mainly caused by just hidden and not destroied data
+        # On change of the type from 'person' to 'organization' or reversed,
+        # we need to delete all the opposide type fields
         if not "type" in vals:
             return {}
         res = {}
@@ -112,25 +108,18 @@ class PartyType(OSV):
             res["name"] = False
         # Reset all person data:
         if vals["type"] == "organization":
-            res["person_last_name"] = False
-            res["person_first_name"] = False
+            res["last_name"] = False
+            res["first_name"] = False
             res["name_order"] = None
         return res
 
-    # Here we take care to resample the name attribute when a name part changes
-    def on_change_person_last_name(self, cursor, user, ids, vals, context=None):
+    def on_change_last_name(self, cursor, user, ids, vals, context=None):
         return self._build_name(cursor, user, ids, vals, context=None)
 
-    def on_change_person_first_name(self, cursor, user, ids, vals, context=None):
+    def on_change_first_name(self, cursor, user, ids, vals, context=None):
         return self._build_name(cursor, user, ids, vals, context=None)
 
     def on_change_name_order(self, cursor, user, ids, vals, context=None):
         return self._build_name(cursor, user, ids, vals, context=None)
-
-    def get_complete_person_name(self, cursor, user, ids, name, args, context=None):
-        res={}
-        for item in self.browse(cursor, user, ids, context=context):
-            res[item.id] = item.name
-        return res
 
 PartyType()
