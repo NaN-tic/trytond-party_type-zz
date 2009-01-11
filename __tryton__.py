36e2bf69d15ef7043cfafe22d4f888ac972b1580
Mathias Behrle <mathiasb@behrle.dyndns.org>
2009-01-11 22:00:33 +0100

    Update translation for de_DE Update and modify translation for release

--- __tryton__.py
+++ __tryton__.py
@@ -2,19 +2,18 @@
 #repository contains the full copyright notices and license terms.
 
 {
-    'name':        'Party Types for People and Organisations',
-    'name_de_DE':  "Geschäftsparteitypen 'Person' und 'Organisation'",
+    'name':        'Party Types',
+    'name_de_DE':  'Partei Typen',
     'version':     '0.0.3',
     'author':      'virtual things',
     'email':       'info@virtual-things.biz',
     'website':     'http://www.virtual-things.biz/',
-    'description': '''Extend party by the types 'person' or
-        'organization' as described by Len Silverston in "The Data Model
-        Resource Book Revised Edition Volume 1" P. 21f''',
-    'description_de_DE': '''Erweitert Geschäftsparteien um die
-        Unterscheidung in 'Organisationen' und 'Personen' wie beschrieben
-        in Silverston Len:"The Data Model Resource Book", Revised Edition,
-        Volume 1, S. 21f.''',
+    'description': '''Extend party by the types 'person' or 'organization'
+    as described by Len Silverston in "The Data Model
+    Resource Book Revised Edition Volume 1" P. 21f''',
+    'description_de_DE': '''Erweitert Geschäftsparteien um die Unterscheidung in 'Organisationen' und 'Personen'
+    wie beschrieben in Silverston Len: "The Data Model Resource Book", Revised Edition, Volume 1, S. 21f."
+''',
     'depends':     ['ir', 'res', 'party',],
     'xml':         [
                      'party_type.xml',
