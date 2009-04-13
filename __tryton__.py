c868ff0df3ba134329dee99347bd1e76880dddee
Udo Spallek <udono@virtual-things.biz>
2009-04-13 21:07:22 +0200

    Make module description more precise. Raises Version number to 1.1.0

--- __tryton__.py
+++ __tryton__.py
@@ -4,16 +4,18 @@
 {
     'name':        'Party Types',
     'name_de_DE':  'Partei Typen',
-    'version':     '0.0.3',
+    'version':     '1.1.0',
     'author':      'virtual things',
     'email':       'info@virtual-things.biz',
     'website':     'http://www.virtual-things.biz/',
-    'description': '''Extend party by the types 'person' or 'organization'
-    as described by Len Silverston in "The Data Model
-    Resource Book Revised Edition Volume 1" P. 21f''',
-    'description_de_DE': '''Erweitert Parteien um die Unterscheidung in 'Organisationen' und 'Personen'
-    wie beschrieben in Silverston Len: "The Data Model Resource Book", Revised Edition, Volume 1, S. 21f."
-''',
+    'description': '''Extend party by the types 'person' or 'organization'.
+        Additionally the Party model for type 'person' is extended by
+        'pre-name' and 'gender' attributes.
+    ''',
+    'description_de_DE': '''Erweitert Parteien um die Unterscheidung in
+        'Organisation' und 'Person'. Zusätzlich ist das Parteien Modell vom
+        Typ 'Person' erweitert mit den Attributen 'Vorname' und 'Geschlecht'.
+    ''',
     'depends':     ['ir', 'res', 'party',],
     'xml':         [
                      'party.xml',
