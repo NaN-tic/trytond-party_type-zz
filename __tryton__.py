53c708cef7c0070c4df9fff3bcb4455f38e5afbc
Udo Spallek <udono@virtual-things.biz>
2009-04-15 10:46:09 +0200

    change module description and indent.

--- __tryton__.py
+++ __tryton__.py
@@ -2,23 +2,25 @@
 #repository contains the full copyright notices and license terms.
 
 {
-    'name':        'Party Types: People and Organizations',
-    'name_de_DE':  'Partei Typen: Personen und Organisationen',
+    'name':        'Distinguish Parties between People and Organizations.',
+    'name_de_DE':  'Parteien in Personen und Organisationen unterscheiden.',
     'version':     '1.1.0',
     'author':      'virtual things',
     'email':       'info@virtual-things.biz',
     'website':     'http://www.virtual-things.biz/',
-    'description': '''Extend party by the types 'person' or 'organization'.
-        Additionally the Party model for type 'person' is extended by
-        'pre-name' and 'gender' attributes.
+    'description': '''
+    Extend party model by the types 'person' or 'organization'. Additionally
+    parties with type 'person' are extended by 'pre-name' and 'gender'
+    attributes.
     ''',
-    'description_de_DE': '''Erweitert Parteien um die Unterscheidung in
-        'Organisation' und 'Person'. Zusätzlich ist das Parteien Modell vom
-        Typ 'Person' erweitert mit den Merkmalen 'Vorname' und 'Geschlecht'.
+    'description_de_DE': '''
+    Erweitert das Modell von Parteien um die Typen 'Person' und
+    'Organisation'. Zusätzlich werden Parteien vom Typ 'Person' erweitert
+    um die Merkmale 'Vorname' und 'Geschlecht'.
     ''',
     'depends':     ['ir', 'res', 'party',],
     'xml':         ['party.xml',],
     'translation': [#'fr_FR.csv',
-                    'de_DE.csv',
-                    #'es_ES.csv',],
+                    #'es_ES.csv',
+                    'de_DE.csv',],
 }
