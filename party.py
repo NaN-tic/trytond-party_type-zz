36d6db7edd4fa195c0c4ecfa166330438697b03c
Korbinian Preisler <timitos@virtual-things.biz>
2009-10-26 21:26:49 +0100

    add search rec_name for search on first_name

--- party.py
+++ party.py
@@ -52,6 +52,27 @@ class Party(ModelSQL, ModelView):
                                                   party.first_name] if x)
         return res
 
+    def search_rec_name(self, cursor, user, name, args, context=None):
+        args2 = []
+        i = 0
+        while i < len(args):
+            ids = self.search(cursor, user, [
+                ('name', args[i][1], args[i][2]),
+                ], limit=1, context=context)
+            if ids:
+                args2.append(('name', args[i][1], args[i][2]))
+            else:
+                ids = self.search(cursor, user, [
+                             ('first_name', args[i][1], args[i][2]),
+                             ], limit=1, context=context)
+                if ids:
+                    args2.append(('first_name', args[i][1], args[i][2]))
+                else:
+                    args2.append((self._rec_name, args[i][1], args[i][2]))
+            i += 1
+        return args2
+
+
     def get_full_name(self, cursor, user, ids, name, arg, context=None):
         """get_full_name(self, cursor, user, ids, name, arg, context=None)
         This method overwrites the standard full name as used in letters or
