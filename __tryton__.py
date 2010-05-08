d1aa28c2143b578b82b7a58430dc1c402fb998b8
Mathias Behrle <mathiasb@mbsolutions.selfip.biz>
2010-05-08 15:09:31 +0200

    Update package description, Guidelines, remove beta warning.

--- __tryton__.py
+++ __tryton__.py
@@ -2,51 +2,29 @@
 #repository contains the full copyright notices and license terms.
 
 {
-    'name':        'BETA: Distinguish Parties between People and Organizations.',
-    'name_de_DE':  'BETA: Parteien in Personen und Organisationen unterscheiden.',
-    'version':     '1.1.0',
-    'author':      'virtual things',
-    'email':       'info@virtual-things.biz',
-    'website':     'http://www.virtual-things.biz/',
-    'description': '''
-    WARNING: BETA STATUS
-This module is in public testing phase and not yet released.
-Never use this module in productive environment. You can not
-uninstall this module once it is installed. Watch
-www.tryton.org/news.html for release announcements.
-
-Use this module only for testing purposes and submit your issues to
-http://bugs.tryton.org. Please note your testing results on
-http://code.google.com/p/tryton/wiki/Testing1_2_0#External_Modules.
-
-
-    Extend party model by the types 'person' or 'organization'. Additionally
-    parties with type 'person' are extended by 'pre-name' and 'gender'
-    attributes.
-    ''',
-    'description_de_DE': '''
-WARNUNG: BETA STATUS
-Dieses Modul befindet sich momentan in der Testphase und ist noch nicht
-veröffentlicht. Dieses Modul ist nicht für Produktivumgebungen
-geeignet. Es ist nicht möglich dieses Modul zu deinstallieren. Achten Sie
-auf Neuigkeiten unter www.tryton.org/news.html.
-
-Helfen Sie bitte dieses Modul zu testen. Eigenarten können unter
-http://bugs.tryton.org gemeldet werden. Bitte vermerken Sie ihre
-Testergebnisse unter
-http://code.google.com/p/tryton/wiki/Testing1_2_0#External_Modules.
-
-
-
-    Erweitert das Modell von Parteien um die Typen 'Person' und
-    'Organisation'. Zusätzlich werden Parteien vom Typ 'Person' erweitert
-    um die Merkmale 'Vorname' und 'Geschlecht'.
-    ''',
-    'depends':     [
-            'party',
-            'company',],
-    'xml':         ['party.xml',],
-    'translation': [#'fr_FR.csv',
-                    #'es_ES.csv',
-                    'de_DE.csv',],
+    'name': 'Party Type',
+    'name_de_DE': 'Parteien Typ',
+    'version': '1.1.0',
+    'author': 'virtual things',
+    'email': 'info@virtual-things.biz',
+    'website': 'http://www.virtual-things.biz/',
+    'description': '''Distinction of Parties between People and Organization
+    - Extends the party model by the types 'person' or 'organization'
+    - Adds attributes 'first name' and 'gender' for parties of type 'person'
+''',
+    'description_de_DE': '''Unterscheidung von Parteien nach Person oder Organisation
+    - Erweitert das Modell von Parteien um die Typen 'Person' und 'Organisation'.
+    - Fügt die Merkmale 'Vorname' und 'Geschlecht' zu Parteien des Typs
+      'Person' hinzu.
+''',
+    'depends': [
+        'party',
+        'company',
+        ],
+    'xml': [
+        'party.xml',
+        ],
+    'translation': [
+        'de_DE.csv',
+        ],
 }
