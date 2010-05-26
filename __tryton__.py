959637e9ded60119a59f85ddb28066c8b73c2303
Mathias Behrle <mathiasb@mbsolutions.selfip.biz>
2010-05-26 13:48:40 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Party Type',
     'name_de_DE': 'Parteien Typ',
-    'version': '1.4.0',
+    'version': '1.5.0',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
