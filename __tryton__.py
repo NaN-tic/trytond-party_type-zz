a5395739fba75711df10d50fc2e4289fc87cf623
Mathias Behrle <mbehrle@m9s.biz>
2010-11-18 21:22:50 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Party Type',
     'name_de_DE': 'Parteien Typ',
-    'version': '1.7.1',
+    'version': '1.7.2',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
