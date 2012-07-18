3a1e0d9021682f95e99c91a226ed1b2b2d442227
resteve <resteve@zikzakmedia.com>
2012-07-18 11:31:23 +0200

    Change copyright notice files

--- __tryton__.py
+++ __tryton__.py
@@ -1,6 +1,6 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this
-#repository contains the full copyright notices and license terms.
-
+#This file is part party_type module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
 {
     'name': 'Party Type',
     'name_ca_ES': 'Tipus de tercers',
