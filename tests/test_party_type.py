5474eb238e61a3128f877a2aeb26059285775bec
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:28:27 +0100

    Cleanup tests

--- tests/test_party_type.py
+++ tests/test_party_type.py
@@ -1,37 +1,26 @@
 #!/usr/bin/env python
-#This file is part party_type module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part party_type module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 import sys, os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class PartyTypeTestCase(unittest.TestCase):
-    '''
-    Test PartyType module.
-    '''
+    'Test PartyType module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('party_type')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('party_type')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 def suite():
