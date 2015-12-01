362c4d4acef33f431bda9d6ce9f07f9e73ed2cb5
resteve <resteve@zikzakmedia.com>
2015-12-01 11:11:58 +0100

    Tests

--- tests/test_party_type.py
+++ tests/test_party_type.py
@@ -1,23 +1,14 @@
-# The COPYRIGHT file at the top level of this repository
-# contains the full copyright notices and license terms.
+# This file is part of the party_type module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartyTypeTestCase(unittest.TestCase):
-    'Test PartyType module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_type')
-
-    def test0005views(self):
-        'Test views'
-        test_view('party_type')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class PartyTypeTestCase(ModuleTestCase):
+    'Test Party Type module'
+    module = 'party_type'
 
 
 def suite():
